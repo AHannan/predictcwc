7d50912f4330dcb60a5771fb2881956afb18f456
Ahannan <a.hannanshahid@yahoo.com>
2019-06-15 04:37:00 +0500

    Dropdown added

--- Cric App/script_basic.js
+++ Cric App/script_basic.js
@@ -48,7 +48,7 @@ function myIP() {
     return false;
 }
 
-function sortTable(n, tableName) {
+function sortTable(n, tableName) {    
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById(tableName);
     switching = true;
@@ -122,4 +122,41 @@ function openHelp(){
     }
     localStorage['close_help'] = 1;
     $('#myModal').modal('show');        
+}
+
+function includes(container, value) {
+	var returnValue = false;
+	var pos = container.indexOf(value);
+	if (pos >= 0) {
+		returnValue = true;
+	}
+	return returnValue;
+}
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
